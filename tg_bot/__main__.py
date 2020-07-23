821e406953eaf3f7502fe0be745ecd7fb4d2c1e8
ShaDisNX <68703998+ShaDisNX@users.noreply.github.com>
2020-07-23 15:00:21 -0500

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -32,7 +32,6 @@ Saya Bot specifically edited by @ShaDisNX255
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm a part of *Eagle Union*
 Have a look at the following for an idea of some of \
 the things I can help you with.
 *Main* commands available:
