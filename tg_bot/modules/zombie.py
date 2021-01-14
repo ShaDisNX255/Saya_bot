56e65232ec828e77897cedd34149092ef6b691c7
ShaDisNX255 <rayo_dis_nx@hotmail.com>
2021-01-14 17:34:59 +0000

    Trying to fix no admin permission rights on Saya

--- tg_bot/modules/zombie.py
+++ tg_bot/modules/zombie.py
@@ -6,7 +6,7 @@ from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins
 
-from tg_bot import telethn, OWNER_ID, DEV_USERS, WHITELIST_USERS, SARDEGNA_USERS
+from tg_bot import dispatcher, OWNER_ID, DEV_USERS, WHITELIST_USERS, SARDEGNA_USERS, SUDO_USERS
 
 # =================== CONSTANT ===================
 
