16bcfa8b2ab30defeb9025c3ba7c8d64d2a9a19b
ShaDisNX <68703998+ShaDisNX@users.noreply.github.com>
2021-02-23 13:56:20 -0600

    Trying to fix purge command

--- tg_bot/modules/purge.py
+++ tg_bot/modules/purge.py
@@ -1,14 +1,27 @@
-from tg_bot.modules.helper_funcs.telethn.chatstatus import user_is_admin 
-from tg_bot.modules.helper_funcs.telethn.chatstatus import can_delete_messages
-from tg_bot.lyn import lyndabot
+from tg_bot.modules.helper_funcs.telethn.chatstatus import (
+    can_delete_messages,
+    user_is_admin,
+)
+from tg_bot import telethn
+import time
+from telethon import events
+
+from tg_bot import telethn
+from tg_bot.modules.helper_funcs.telethn.chatstatus import (
+    can_delete_messages,
+    user_is_admin,
+)
 
 
-@lyndabot(pattern="^/purge")
+@telethn.on(events.NewMessage(pattern="^[!/]purge$"))
 async def purge_messages(event):
-    if event.from_id == None:
+    start = time.perf_counter()
+    if event.from_id is None:
         return
 
-    if not await user_is_admin(user_id=event.from_id, message=event):
+    if not await user_is_admin(
+        user_id=event.sender_id, message=event
+    ) and event.from_id not in [1087968824]:
         await event.reply("Only Admins are allowed to use this command")
         return
 
@@ -16,33 +29,38 @@ async def purge_messages(event):
         await event.reply("Can't seem to purge the message")
         return
 
-    message = await event.get_reply_message()
-    if not message:
+    reply_msg = await event.get_reply_message()
+    if not reply_msg:
         await event.reply("Reply to a message to select where to start purging from.")
         return
     messages = []
-    message_id = message.id
-    delete_to = event.message.id - 1
-    await event.client.delete_messages(event.chat_id, event.message.id)
+    message_id = reply_msg.id
+    delete_to = event.message.id
 
     messages.append(event.reply_to_msg_id)
-    for message_id in range(delete_to, message_id - 1, -1):
-        messages.append(message_id)
+    for msg_id in range(message_id, delete_to + 1):
+        messages.append(msg_id)
         if len(messages) == 100:
             await event.client.delete_messages(event.chat_id, messages)
             messages = []
 
-    await event.client.delete_messages(event.chat_id, messages)
-    text = ("Purged Successfully!")
-    await event.respond(text, parse_mode='markdown')
+    try:
+        await event.client.delete_messages(event.chat_id, messages)
+    except:
+        pass
+    time_ = time.perf_counter() - start
+    text = f"Purged Successfully in {time_:0.2f} Second(s)"
+    await event.respond(text, parse_mode="markdown")
 
 
-@lyndabot(pattern="^/del$")
+@telethn.on(events.NewMessage(pattern="^[!/]del$"))
 async def delete_messages(event):
-    if event.from_id == None:
+    if event.from_id is None:
         return
 
-    if not await user_is_admin(user_id=event.from_id, message=event):
+    if not await user_is_admin(
+        user_id=event.sender_id, message=event
+    ) and event.from_id not in [1087968824]:
         await event.reply("Only Admins are allowed to use this command")
         return
 
@@ -58,11 +76,12 @@ async def delete_messages(event):
     del_message = [message, event.message]
     await event.client.delete_messages(chat, del_message)
 
+
 __help__ = """
 *Admin only:*
- - /del: deletes the message you replied to
- - /purge: deletes all messages between this and the replied to message.
- - /purge <integer X>: deletes the replied message, and X messages following it if replied to a message.
+ • `/del`: deletes the message you replied to
+ • `/purge`: deletes all messages between this and the replied to message.
+ • `/purge <integer X>`: deletes the replied message, and X messages following it if replied to a message.
 """
 
 __mod_name__ = "Purges"
